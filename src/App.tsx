5ce8d6062a0a4813254b1f28038160faf9989232
Amol Jagtap <amoljagtap17@gmail.com>
2023-04-03 13:37:20 +0530

    2023-docker-demo: add environments to docker compose

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Docker!!
+          React with Docker 2 deleted!!
         </a>
       </header>
     </div>
